215dcf4921daa0b753881166cf93ebd8dcd91870
mdsalahuddin96 <salauddincse96@gmail.com>
2026-02-18 10:55:29 +0600

    added interactivity using js

--- login.js
+++ login.js
@@ -1,1 +1,18 @@
-console.log("connected")
+// get button and input element
+const loginBtn=document.getElementById('login-btn');
+const inputNumber=document.getElementById('number-input')
+const inputPassword=document.getElementById('password-input')
+
+// add eventlistener to button
+loginBtn.addEventListener('click',function(){
+    const mobileNumber=inputNumber.value;
+    const pin=inputPassword.value;
+    if(mobileNumber=="01712345678"&&pin=="1234"){
+        alert('login successful');
+        window.location.assign('home.html')
+    }
+    else{
+        alert("Login Failed")
+        return;
+    }
+})
